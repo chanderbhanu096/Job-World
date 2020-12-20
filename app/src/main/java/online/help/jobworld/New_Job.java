24dd33a703f580fdd5b41023840236d8a6b2a016
Chander Bhanu <chanderbhanu096@gmail.com>
2020-12-20 21:00:04 +0530

    Second Commit

--- app/src/main/java/online/help/jobworld/New_Job.java
+++ app/src/main/java/online/help/jobworld/New_Job.java
@@ -4,22 +4,38 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class New_Job extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    ;
+    Button submitJob;
+    EditText title, category, description, jobLocation, contactDetails;
+    Spinner aSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_new__job );
-        Spinner aSpinner = findViewById( R.id.spinner );
+
+        submitJob = (Button) findViewById( R.id.submit_new_job );
+        title = (EditText) findViewById( R.id.title );
+        description = (EditText) findViewById( R.id.job_description );
+        jobLocation = (EditText) findViewById( R.id.job_location );
+        contactDetails = (EditText) findViewById( R.id.contact_me );
+
+        aSpinner = findViewById( R.id.spinner );
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource( this, R.array.Spinner_item, android.R.layout.simple_spinner_item );
         adapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
         aSpinner.setAdapter( adapter );
         aSpinner.setOnItemSelectedListener( this );
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     }
 

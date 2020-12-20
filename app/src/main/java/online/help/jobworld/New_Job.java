7bf7f8f676451ec3f94f0112c40b4ca1d3d0a9df
Chander Bhanu <chanderbhanu096@gmail.com>
2020-12-20 16:27:13 +0530

    Second Commit

--- app/src/main/java/online/help/jobworld/New_Job.java
+++ app/src/main/java/online/help/jobworld/New_Job.java
@@ -1,14 +1,36 @@
 package online.help.jobworld;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class New_Job extends AppCompatActivity {
+public class New_Job extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_new__job );
+        Spinner aSpinner = findViewById( R.id.spinner );
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource( this, R.array.Spinner_item, android.R.layout.simple_spinner_item );
+        adapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
+        aSpinner.setAdapter( adapter );
+        aSpinner.setOnItemSelectedListener( this );
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition( position ).toString();
+        Toast.makeText( parent.getContext(), text, Toast.LENGTH_SHORT ).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
